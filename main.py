109a41c04c1316d54ec52e53f1c6234686a81b60
zntb <zenetibi@yahoo.com>
2025-07-24 09:04:21 +0300

    Action when the Webcam Detects an Object

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import cv2
 import time
+from emailing import send_email
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
+status_list = []
  
 while True:
-   
+    status = 0
     check, frame = video.read()
     gray_frame  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau  = cv2.GaussianBlur(gray_frame, (21, 21), 0)
@@ -29,9 +31,18 @@ while True:
         if cv2.contourArea(contour) < 5000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
         cv2.putText(frame, "Status: {}".format('Movement'), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-        cv2.imshow("My video", frame)
+        if rectangle.any():
+            status = 1
+            
+    status_list.append(status)
+    status_list = status_list[-2:]
+    if status_list[0] == 1 and status_list[1] == 0:
+        # time.sleep(5)
+        send_email()
+            
+    cv2.imshow("My video", frame)
     
     key = cv2.waitKey(1)
     
